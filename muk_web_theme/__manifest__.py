6deba1baa216b071a38f84a7e8f395e66379a4c2
Ashwinppragtech <ashwin.p@pragtech.co.in>
2024-06-13 17:07:18 +0530

    [EDITED] grafana folder copying command111111111111111

--- muk_web_theme/__manifest__.py
+++ muk_web_theme/__manifest__.py
@@ -12,7 +12,7 @@
 {
     'name': 'MuK Backend Theme', 
     'summary': 'Odoo Community Backend Theme',
-    'version': '16.0.1.0.9',
+    'version': '16.0.1.0.10',
     'category': 'Themes/Backend', 
     'license': 'LGPL-3', 
     'author': 'MuK IT',
